d01a7c02f0d5073a05706c9a2730261c9412bd40
Paul Marques Mota <paul@marquesmota.pt>
2019-02-04 11:18:55 +0100

    classe de jeu

---
+++ src/pt/marquesmota/Game.java
@@ -0,0 +1,7 @@
+package pt.marquesmota;
+
+public class Game {
+	public static void start() {
+		System.out.println("Hello world!");
+	}
+}
---
+++ src/pt/marquesmota/Main.java
@@ -0,0 +1,7 @@
+package pt.marquesmota;
+
+public class Main {
+	public static void main(String[] args) {
+		Game.start();
+	}
+}
