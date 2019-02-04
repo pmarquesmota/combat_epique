804d8f5b89777f4395c29acc9838e39a2ee74e10
Paul Marques Mota <paul@marquesmota.pt>
2019-02-04 15:06:13 +0100

    attaques spéciales

--- src/pt/marquesmota/Mage.java
+++ src/pt/marquesmota/Mage.java
@@ -5,6 +5,12 @@ public class Mage extends Joueur{
 		return new Effet("Boule de Feu", 0, Intelligence);
 	}
 	public Effet attaque_speciale() {
+		int gain = Intelligence*2;
+		Vie += gain;
+		if(Vie > VieInitiale) {
+			Vie = VieInitiale;
+		}
+		System.out.println(Nom+" se soigne et regagne "+gain+" points de vie");
 		return new Effet("", 0, 0);
 	}
 
--- src/pt/marquesmota/Rodeur.java
+++ src/pt/marquesmota/Rodeur.java
@@ -5,6 +5,8 @@ public class Rodeur extends Joueur{
 		return new Effet("Tir à l'Arc", 0, Agilite);
 	}
 	public Effet attaque_speciale() {
+		Agilite += Niveau/2;
+		System.out.println(Nom + " se concentre et gagne "+Niveau/2+" en agilité.");
 		return new Effet("", 0, 0);
 	}
 
