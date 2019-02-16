3d1a1388f07ea8c7077f6a525401e7d7773c1395
Paul Marques Mota <paul@marquesmota.pt>
2019-02-16 10:28:15 +0100

    refactorisation

--- src/pt/marquesmota/Game.java
+++ src/pt/marquesmota/Game.java
@@ -4,53 +4,58 @@ import java.util.Scanner;
 
 /**
  * class which is used to manage the abstract high level view of the game.
- * @author      Paul Marques Mota <pmarquesmota@gmail.com>
- * @version     1.0
+ * 
+ * @author Paul Marques Mota <pmarquesmota@gmail.com>
+ * @version 1.0
  */
 public class Game {
 	/**
-	 * This variable is declared once here to avoid being instanced each time Choose.choice is called
+	 * This variable is declared once here to avoid being instanced each time
+	 * Choose.choice is called
 	 */
 	static Scanner sc;
-	
+
+	private int Classe;
+	private int Niveau;
+	private int Force;
+	private int Agilite;
+	private int Intelligence;
+
 	/**
-	 * Creates the characters and pass the execution to run_game to carry on to the main loop of the game.
+	 * Creates the characters and pass the execution to run_game to carry on to the
+	 * main loop of the game.
 	 */
-	public static void start() {
+	public void start() {
 		Joueur Joueur1, Joueur2;
 
 		sc = new Scanner(System.in);
-		
-		System.out.println("Création du personnage du Joueur 1");
-		Joueur1 = create_character("Joueur 1");
-		System.out.println(Joueur1.toString());
 
-		System.out.println("Création du personnage du Joueur 2");
+		Joueur1 = create_character("Joueur 1");
 		Joueur2 = create_character("Joueur 2");
-		System.out.println(Joueur2.toString());
 
 		run_game(Joueur1, Joueur2);
 	}
-	
+
 	/**
 	 * The main loop.
 	 * <p>
 	 * the characters attack each other until one of them is dead
 	 * </p>
+	 * 
 	 * @param joueur1 the first character
 	 * @param joueur2 the second character
 	 */
-	private static void run_game(Joueur joueur1, Joueur joueur2) {
+	private void run_game(Joueur joueur1, Joueur joueur2) {
 		while (joueur1.getVie() > 0 && joueur2.getVie() > 0) {
 			joueur1.attaque(joueur2);
-			if(joueur1.getVie()>0 && joueur2.getVie()>0) {
+			if (joueur1.getVie() > 0 && joueur2.getVie() > 0) {
 				joueur2.attaque(joueur1);
 			}
 		}
-		if(joueur1.getVie() <= 0) {
+		if (joueur1.getVie() <= 0) {
 			System.out.println("Joueur 1 a perdu !");
 		} else {
-			System.out.println("Joueur 2 a perdu !");			
+			System.out.println("Joueur 2 a perdu !");
 		}
 	}
 
@@ -60,17 +65,33 @@ public class Game {
 	 * The player is asked to choose a name and characteristics of her character.
 	 * Then a new character object is created according the player specifications.
 	 * </p>
+	 * 
 	 * @param nouveauNom the name of the character.
 	 * @return an object representing the character chosen.
 	 */
-	public static Joueur create_character(String nouveauNom) {
+	public Joueur create_character(String nouveauNom) {
+		Joueur unJoueur;
+		System.out.println("Création du personnage du " + nouveauNom);
+
+		choose_character();
+		unJoueur = create_character_object(nouveauNom);
+		System.out.println(unJoueur.toString());
+
+		return unJoueur;
+	}
+
+	public void choose_character() {
+		Classe = Choose.choice("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)", 1,
+				3);
+		Niveau = Choose.choice("Niveau du personnage ?", 1, 100);
+		Force = Choose.choice("Force du personnage ?", 0, 100);
+		Agilite = Choose.choice("Agilite du personnage ?", 0, 100);
+		Intelligence = Choose.choice("Intelligence du personnage ?", 0, 100);
+	}
+
+	public Joueur create_character_object(String nouveauNom) {
 		Joueur nouveauJoueur = null;
 
-		int Classe = Choose.choice("Veuillez choisir la classe de votre personnage (1 : Guerrier, 2 : Rôdeur, 3 : Mage)", 1, 3);
-		int Niveau = Choose.choice("Niveau du personnage ?", 1, 100);
-		int Force = Choose.choice("Force du personnage ?", 0, 100);
-		int Agilite = Choose.choice("Agilite du personnage ?", 0, 100);
-		int Intelligence = Choose.choice("Intelligence du personnage ?", 0, 100);
 		switch (Classe) {
 		case 1:
 			nouveauJoueur = new Guerrier(nouveauNom, Niveau, Niveau * 5, Force, Agilite, Intelligence);
--- src/pt/marquesmota/Main.java
+++ src/pt/marquesmota/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		Game.start();
+		Game g = new Game();
+		g.start();
 	}
 }
