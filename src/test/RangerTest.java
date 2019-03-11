aef3a7671db0034861d74028d8b63853a24e87a7
Paul Marques Mota <paul@marquesmota.pt>
2019-03-11 19:04:24 +0100

    comments

--- src/test/RangerTest.java
+++ src/test/RangerTest.java
@@ -13,12 +13,18 @@ import pt.marquesmota.*;
 
 public class RangerTest {
 	
+	/**
+	 * Test the toString method.	
+	 */
 	@Test
 	public void test_toString() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
 		assertEquals(j.toString(),"Woof je suis le Rodeur Joueur 1 niveau 10 je possède 50 de vitalité, 0 de force, 10 d'agilité et 0 d'intelligence !");
 	}
 	
+	/**
+	 * Test the special_attack method, which implements the special attack of the character.	
+	 */
 	@Test
 	public void test_special_attack() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
@@ -30,6 +36,9 @@ public class RangerTest {
 		assertEquals(e.getOtherDamage(), 0);
 	}
 	
+	/**
+	 * Test the basic_attack method, which implements the basic attack of the character.	
+	 */
 	@Test
 	public void test_basic_attack() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
@@ -39,6 +48,9 @@ public class RangerTest {
 		assertEquals(e.getOtherDamage(), 10);
 	}
 	
+	/**
+	 * Test the loseLife method when there is no life loss to the character.
+	 */
 	@Test
 	public void test_loseLife_nothing() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("");
@@ -52,6 +64,9 @@ public class RangerTest {
 		assertEquals(baos.toString(), "");
 	}
 	
+	/**
+	 * Test the loseLife method when there is some life loss to the character.
+	 */
 	@Test
 	public void test_loseLife_little() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("");
@@ -65,6 +80,9 @@ public class RangerTest {
 		assertEquals(baos.toString(), "Joueur 1 perd 10 points de vie\n");
 	}
 	
+	/**
+	 * Test the loseLife method when the character dies.
+	 */
 	@Test
 	public void test_loseLife_all() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("");
@@ -78,6 +96,9 @@ public class RangerTest {
 		assertEquals(baos.toString(), "Joueur 1 perd 50 points de vie\nJoueur 1 est mort\n");
 	}
 	
+	/**
+	 * Test the generic basic attack.
+	 */
 	@Test
 	public void test_attack_basic_attack() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("1\n");
@@ -95,6 +116,9 @@ public class RangerTest {
 		assertEquals(baos.toString(), "Joueur 1 (50 Vitalité) veuillez choisir votre action (1 : Attaque Basique, 2 : Attaque Spéciale)\nJoueur 1 utilise Tir à l'Arc et inflige 10 dommages.\nJoueur 2 perd 10 points de vie\n");
 	}
 	
+	/**
+	 * Test the generic special attack.
+	 */
 	@Test
 	public void test_attack_special_attack() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("2\n");
@@ -112,6 +136,9 @@ public class RangerTest {
 		assertEquals(baos.toString(), "Joueur 1 (50 Vitalité) veuillez choisir votre action (1 : Attaque Basique, 2 : Attaque Spéciale)\nJoueur 1 utilise Concentration et gagne 5 en agilité.\n");
 	}
 	
+	/**
+	 * Test the setter and getter for the Life variable
+	 */
 	@Test
 	public void test_setVie_getVie() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
@@ -119,24 +146,36 @@ public class RangerTest {
 		assertEquals(j.getLife(), 60);
 	}
 	
+	/**
+	 * Test the getter for the InitialLife variable
+	 */
 	@Test
 	public void test_getVieInitiale() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
 		assertEquals(j.getInitialLife(), 50);
 	}
 	
+	/**
+	 * Test the getter for the Level variable
+	 */
 	@Test
 	public void test_getNiveau() {
 		Ranger j = new Ranger("Joueur 1", 10, 50,0, 10, 0);
 		assertEquals(j.getLevel(), 10);
 	}
 	
+	/**
+	 * Test the getter for the Intelligence variable
+	 */
 	@Test
 	public void test_getIntelligence() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
 		assertEquals(j.getIntelligence(), 0);
 	}
 	
+	/**
+	 * Test the setter and getter for the Agility variable
+	 */
 	@Test
 	public void test_setAgilite_getAgilite() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
@@ -144,12 +183,18 @@ public class RangerTest {
 		assertEquals(j.getAgility(), 20);
 	}
 	
+	/**
+	 * Test the getter for the Strength variable
+	 */
 	@Test
 	public void test_getForce() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
 		assertEquals(j.getStrength(), 0);
 	}
 	
+	/**
+	 * Test the getter for the Name variable
+	 */
 	@Test
 	public void test_getNom() {
 		Ranger j = new Ranger("Joueur 1", 10, 50, 0, 10, 0);
