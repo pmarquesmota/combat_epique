94884aa9d80e0d8629f81cfbc2d5e9e24fdf53e7
Paul Marques Mota <paul@marquesmota.pt>
2019-03-11 19:25:15 +0100

    comments

--- src/test/WizardTest.java
+++ src/test/WizardTest.java
@@ -13,12 +13,18 @@ import pt.marquesmota.*;
 
 public class WizardTest {
 	
+	/**
+	 * Test the toString method.	
+	 */
 	@Test
 	public void test_toString() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
 		assertEquals(j.toString(),"Abracadabra je suis le Mage Joueur 1 niveau 10 je possède 50 de vitalité, 0 de force, 0 d'agilité et 10 d'intelligence !");
 	}
 	
+	/**
+	 * Test the special_attack method, which implements the special attack of the character.	
+	 */
 	@Test
 	public void test_special_attack() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
@@ -28,6 +34,9 @@ public class WizardTest {
 		assertEquals(e.getOtherDamage(), 0);
 	}
 	
+	/**
+	 * Test the basic_attack method, which implements the basic attack of the character.	
+	 */
 	@Test
 	public void test_basic_attack() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
@@ -37,6 +46,9 @@ public class WizardTest {
 		assertEquals(e.getOtherDamage(), 10);
 	}
 	
+	/**
+	 * Test the loseLife method when there is no life loss to the character.
+	 */
 	@Test
 	public void test_loseLife_nothing() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("");
@@ -52,6 +64,9 @@ public class WizardTest {
 		assertEquals(baos.toString(), "");
 	}
 	
+	/**
+	 * Test the loseLife method when there is some life loss to the character.
+	 */
 	@Test
 	public void test_loseLife_little() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("");
@@ -67,6 +82,9 @@ public class WizardTest {
 		assertEquals(baos.toString(), "Joueur 1 perd 10 points de vie\n");
 	}
 	
+	/**
+	 * Test the loseLife method when the character dies.
+	 */
 	@Test
 	public void test_loseLife_all() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("");
@@ -82,6 +100,9 @@ public class WizardTest {
 		assertEquals(baos.toString(), "Joueur 1 perd 50 points de vie\nJoueur 1 est mort\n");
 	}
 	
+	/**
+	 * Test the generic basic attack.
+	 */
 	@Test
 	public void test_attack_basic_attack() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("1\n");
@@ -99,6 +120,9 @@ public class WizardTest {
 		assertEquals(baos.toString(), "Joueur 1 (50 Vitalité) veuillez choisir votre action (1 : Attaque Basique, 2 : Attaque Spéciale)\nJoueur 1 utilise Boule de Feu et inflige 10 dommages.\nJoueur 2 perd 10 points de vie\n");
 	}
 	
+	/**
+	 * Test the generic special attack.
+	 */
 	@Test
 	public void test_attack_special_attack() {
 		ByteArrayOutputStream baos = MyStream.RedirectStream("2\n");
@@ -116,6 +140,9 @@ public class WizardTest {
 		assertEquals(baos.toString(), "Joueur 1 (50 Vitalité) veuillez choisir votre action (1 : Attaque Basique, 2 : Attaque Spéciale)\nJoueur 1 utilise Soin et gagne 20 en vitalité\n");
 	}
 	
+	/**
+	 * Test the setter and getter for the Life variable
+	 */
 	@Test
 	public void test_setVie_getVie() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
@@ -123,24 +150,36 @@ public class WizardTest {
 		assertEquals(j.getLife(), 60);
 	}
 	
+	/**
+	 * Test the getter for the InitialLife variable
+	 */
 	@Test
 	public void test_getVieInitiale() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
 		assertEquals(j.getInitialLife(), 50);
 	}
 	
+	/**
+	 * Test the getter for the Level variable
+	 */
 	@Test
 	public void test_getNiveau() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
 		assertEquals(j.getLevel(), 10);
 	}
 	
+	/**
+	 * Test the getter for the Intelligence variable
+	 */
 	@Test
 	public void test_getIntelligence() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
 		assertEquals(j.getIntelligence(), 10);
 	}
 	
+	/**
+	 * Test the setter and getter for the Agility variable
+	 */
 	@Test
 	public void test_setAgilite_getAgilite() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
@@ -148,12 +187,18 @@ public class WizardTest {
 		assertEquals(j.getAgility(), 20);
 	}
 	
+	/**
+	 * Test the getter for the Strength variable
+	 */
 	@Test
 	public void test_getForce() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
 		assertEquals(j.getStrength(), 0);
 	}
 	
+	/**
+	 * Test the getter for the Name variable
+	 */
 	@Test
 	public void test_getNom() {
 		Wizard j = new Wizard("Joueur 1", 10, 50, 0, 0, 10);
